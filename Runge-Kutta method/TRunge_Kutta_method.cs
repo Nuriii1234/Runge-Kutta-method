44ab78b87d30abd87490e8a77a9b011fe23ab9fa
Салават Нуриев <nurievsalav@yandex.ru>
2023-12-29 20:46:38 +0300

    Более подробное описание

--- Runge-Kutta method/TRunge_Kutta_method.cs
+++ Runge-Kutta method/TRunge_Kutta_method.cs
@@ -36,8 +36,6 @@ namespace Runge_Kutta_method
             vector_K[1] = h * Function(x + 0.5 * h , y + 0.5 * vector_K[0]);
             vector_K[2] = h * Function(x + 0.5 * h, y + 0.5 * vector_K[1]);
             vector_K[3] = h * Function(x + h, y + vector_K[2]);
-            Console.WriteLine("Vector K:");
-            PrintVector(vector_K);
             return vector_K;
         }
         void Method()
@@ -47,8 +45,8 @@ namespace Runge_Kutta_method
             double y = 0.541325;
             double [] vector_x = new double[k];
             double [] vector_y = new double[k];
-            double[] vector_original_y = new double[k];
-            double [] vector_Eps = new double[k];
+            double[] vector_original_y = new double[k - 1];
+            double [] vector_Eps = new double[k - 1];
             vector_x[0] = x;
             vector_y[0] = y;
             double h = 0.1;
@@ -60,20 +58,19 @@ namespace Runge_Kutta_method
                 vector_original_y[i] = Original_Function(x);
                 delta_Y = (vector_K[0] + 2 * vector_K[1] + 2 * vector_K[2] + vector_K[3]) / 6;
                 y += delta_Y;
-                Console.WriteLine(" delta Y = " + delta_Y);
                 x += h;
                 vector_Eps[i] = Math.Abs(y - Original_Function(x));
                 i++;
                 vector_x[i] = x;
                 vector_y[i] = y;
             }
-            Console.WriteLine("Vector X:");
+            Console.WriteLine("Значения Х:");
             PrintVector(vector_x);
-            Console.WriteLine("Vector Y:");
+            Console.WriteLine("Значения У по методу Рунге-Кутта:");
             PrintVector(vector_y);
-            Console.WriteLine("Vector original Y:");
+            Console.WriteLine("Значения У по точному уравнению:");
             PrintVector(vector_original_y);
-            Console.WriteLine("Vector Eps:");
+            Console.WriteLine("Ошибка У для каждого Х:");
             PrintVector(vector_Eps);
         }
         public void Start()
